2c8d6283c596d7ef55c3279f73b47518bf0788cb
Warith HARCHAOUI <warith-harchaoui@users.noreply.github.com>
2024-10-11 11:30:16 +0200

    Update __init__.py

--- sftp_helper/__init__.py
+++ sftp_helper/__init__.py
@@ -1,12 +1,25 @@
 """
 SFTP Helper
 
-This module provides various helper functions to interact with an SFTP server.
-It includes functionality for uploading, downloading, deleting, and verifying the existence of remote files via SFTP.
+This module provides functions to interact with an SFTP server, allowing users to perform file uploads, downloads, 
+and deletions, as well as to check file existence remotely. The module relies on pysftp for SFTP operations and 
+os_helper for auxiliary tasks.
 
 Authors:
-- Warith Harchaoui <warith.harchaoui@deraison.ai>
+- [Warith Harchaoui](https://harchaoui.org/warith)
+- [Mohamed Chelali](https://mchelali.github.io)
+- [Bachir Zerroug](https://www.linkedin.com/in/bachirzerroug)
 """
+
+__all__ = [
+    'credentials',
+    'get_client_sftp',
+    'strip_sftp_path',
+    'remote_file_exists',
+    'delete',
+    'upload',
+    'download'
+]
 
 from .main import (
     credentials,
