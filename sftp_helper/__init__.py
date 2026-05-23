7f93fe5721fe946ac2a41b0862cbc3b8e3956f4f
Warith HARCHAOUI <warith.harchaoui@gmail.com>
2026-05-23 07:47:07 +0200

    refactor: replace pysftp with paramiko to enable strict host-key verification and add remote tempfile utility

--- sftp_helper/__init__.py
+++ sftp_helper/__init__.py
@@ -1,37 +1,40 @@
 """
 SFTP Helper
 
-This module provides functions to interact with an SFTP server, allowing users to perform file uploads, downloads, 
-and deletions, as well as to check file existence remotely. The module relies on pysftp for SFTP operations and 
-os_helper for auxiliary tasks.
+This module provides functions to interact with an SFTP server, allowing users
+to perform file uploads, downloads, and deletions, as well as to check file
+existence remotely. Backed by paramiko with strict host-key verification.
 
 Authors:
-- [Warith Harchaoui](https://harchaoui.org/warith)
-- [Mohamed Chelali](https://mchelali.github.io)
-- [Bachir Zerroug](https://www.linkedin.com/in/bachirzerroug)
+- Warith Harchaoui (https://harchaoui.org/warith)
+- Mohamed Chelali (https://mchelali.github.io)
+- Bachir Zerroug (https://www.linkedin.com/in/bachirzerroug)
 """
 
 __all__ = [
-    'credentials',
-    'get_client_sftp',
-    'strip_sftp_path',
-    'remote_file_exists',
-    'delete',
-    'upload',
-    'download',
-    'remote_dir_exist',
-    'make_remote_directory',
+    "credentials",
+    "get_client_sftp",
+    "normalize_path",
+    "strip_sftp_path",
+    "remote_file_exists",
+    "delete",
+    "upload",
+    "download",
+    "remote_dir_exist",
+    "make_remote_directory",
+    "remote_tempfile",
 ]
 
 from .main import (
     credentials,
-    get_client_sftp,
-    strip_sftp_path,
-    remote_file_exists,
     delete,
-    upload,
     download,
-    remote_dir_exist,
+    get_client_sftp,
     make_remote_directory,
+    normalize_path,
+    remote_dir_exist,
+    remote_file_exists,
+    remote_tempfile,
+    strip_sftp_path,
+    upload,
 )
-
